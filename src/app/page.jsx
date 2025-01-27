503e74e42b4feb6a6eb7828c920e5ba0527136ea
Jesus Ruiz <javier.ruiz2506@gmail.com>
2025-01-27 16:52:15 -0400

    change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -63,7 +63,7 @@ export default function Home() {
     <>
       <nav className={`${isSticky ? 'sticky' : ''} ${styles.navbar_bg}`}>
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-code"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M7 8l-4 4l4 4" /><path d="M17 8l4 4l-4 4" /><path d="M14 4l-4 16" /></svg>
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portafolio</span>
           </a>
